7b15d3dd49a3387e754d0bfcc8f2946f30765872
GabeButler01 <89435460+GabeButler01@users.noreply.github.com>
2025-04-25 18:37:58 -0600

    Update CapstonePhone.py

--- CapstonePhone.py
+++ CapstonePhone.py
@@ -6,8 +6,8 @@ from twilio.rest import Client
 
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
-account_sid = "████████████████████████████████" # Account SID (use secrets file)
-auth_token = "████████████████████████████████" # Auth Token (use secrets file)
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
 client = Client(account_sid, auth_token)
 
 # list of voicemails recordings
